10db4e2b0aa9385d2dd3d66e425670135bd973e3
mistarson <yeowuli2@naver.com>
2023-12-04 11:19:32 +0900

    TestConfig: Controller 테스트를 위한 설정을 위한 securityFilter 구현

---
+++ src/test/java/F12/newsfeedproject/filter/MockSpringSecurityFilter.java
@@ -0,0 +1,28 @@
+package F12.newsfeedproject.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class MockSpringSecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) request).getUserPrincipal());
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
