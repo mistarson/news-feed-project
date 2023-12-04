eb5ef070a279c63fa06614445e3a6f7d6513f40e
mistarson <yeowuli2@naver.com>
2023-12-04 11:14:52 +0900

    Test: CommentService 테스트

--- src/main/java/F12/newsfeedproject/domain/comment/service/CommentService.java
+++ src/main/java/F12/newsfeedproject/domain/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package F12.newsfeedproject.domain.comment.service;
 
     import F12.newsfeedproject.domain.comment.entity.Comment;
     import F12.newsfeedproject.domain.comment.repository.CommentRepository;
+    import java.util.Optional;
     import lombok.RequiredArgsConstructor;
     import org.springframework.stereotype.Service;
 
@@ -15,11 +16,15 @@ public class CommentService {
         return commentRepository.save(comment);
     }
 
-    public Comment findByCommentId(Long commentId) {
-        return commentRepository.findById(commentId).orElseThrow(IllegalArgumentException::new);
+    public Optional<Comment> findByCommentId(Long commentId) {
+        return commentRepository.findById(commentId);
     }
 
     public void deleteComment(Long commentId) {
         commentRepository.deleteById(commentId);
+    }
+
+    public void updateComment(Comment comment, String commentContent) {
+        comment.updateCommentContent(commentContent);
     }
 }
